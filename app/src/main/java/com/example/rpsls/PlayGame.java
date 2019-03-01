664bc6ffd338429d55f5f6cb6f778f0bfa6ab084
Andrei Stoyanow <stoyanowaa@hendrix.edu>
2019-02-28 22:41:16 -0600

    Changes to Play Game and Game Screen classes
    Added some methods (unfinished) to PlayGame and GameScreen, still need to work on connecting with server and that stuff. Trying to send move thru client socket and trying to find a way to tie in calculation class with these two classes.

--- app/src/main/java/com/example/rpsls/PlayGame.java
+++ app/src/main/java/com/example/rpsls/PlayGame.java
@@ -1,6 +1,10 @@
 package com.example.rpsls;
 
+import android.util.Log;
+import android.widget.TextView;
+
 import java.io.IOException;
+import java.net.Socket;
 
 public class PlayGame {
 
@@ -9,16 +13,36 @@ public class PlayGame {
             @Override
             public void run() {
             }
-
         });
         try{
-            Server s = new Server();
-            //s.addListener
+            Server.get().addListener(new ServerListener() {
+                @Override
+                public void notifyConnection(String host) {
 
+                }
+            });
+            Server.get().listen();
         }
         catch (IOException e){
-
+            Log.e(GameScreen.class.getName(), "Could not connect to server");
         }
     }
+
+    public static void sendMove(final String move, final String host, final int port){
+        new Thread(){
+            @Override
+            public void run(){
+                try{
+                    Socket target = new Socket(host, port);
+                    Connection.broadcast(target, move);
+                    String opponentsMove = Connection.receive(target);
+                }
+                catch(final Exception e){
+
+                }
+            }
+        };
+    }
+
 
 }
