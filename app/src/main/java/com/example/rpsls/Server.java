980cb5940e047f7fd556a3288e8167e4516f84ed
Blake <blake.hawkins97@gmail.com>
2019-03-05 23:35:08 -0600

    Update Server.java
    Trying to fix invite bug.

--- app/src/main/java/com/example/rpsls/Server.java
+++ app/src/main/java/com/example/rpsls/Server.java
@@ -33,8 +33,8 @@ public class Server {
 
     public void listen() throws IOException {
         for (;;) {
-            listenForConnections().run();
             listenForInviteResolutions().run();
+            listenForConnections().run();
         }
     }
 
