2b7902adaa25df7367ea1bae1f5c9e599d30ceb9
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-03-06 16:07:32 -0600

    Create HandlingMoves.java

---
+++ app/src/main/java/com/example/rpsls/HandlingMoves.java
@@ -0,0 +1,16 @@
+package com.example.rpsls;
+
+import android.util.Log;
+
+public class HandlingMoves {
+
+    public static void TheGame(String hostIP, String oppponentIP){
+        String clientMove = hostIP;
+        Log.d("GameScreen", "notifyConnection: " + target);
+        setOpponentMoveToTextView(clientMove, opponentMove);
+        Log.d("GameScreen: ","notifyConnection: " + opponentMove);
+        game.calculateWinner(userMove.getText().toString(),opponentMove.getText().toString(),result );
+
+
+    }
+}
